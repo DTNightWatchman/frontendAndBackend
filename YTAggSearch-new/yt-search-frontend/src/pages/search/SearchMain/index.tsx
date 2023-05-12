1b8b3bf3bc2439c02b841389b8fa6b2f93d510f0
lenovo <2975545458@qq.com>
2023-05-12 19:37:13 +0800

    前端框架搭建

---
+++ YTAggSearch-new/yt-search-frontend/src/pages/search/SearchMain/index.tsx
@@ -0,0 +1,22 @@
+import React  from 'react';
+// const LoginMessage: React.FC<{
+//   content: string;
+// }> = ({ content }) => (
+//   <Alert
+//     style={{
+//       marginBottom: 24,
+//     }}
+//     message={content}
+//     type="error"
+//     showIcon
+//   />
+// );
+const SearchMain: React.FC = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+export default SearchMain;
