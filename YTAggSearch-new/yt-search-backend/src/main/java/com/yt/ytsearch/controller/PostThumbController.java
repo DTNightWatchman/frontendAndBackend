5ec573d63c59e1d7faf478d555fb5dc56945c538
lenovo <2975545458@qq.com>
2023-05-13 17:14:56 +0800

    前后端框架搭建

---
+++ YTAggSearch-new/yt-search-backend/src/main/java/com/yt/ytsearch/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.yt.ytsearch.controller;
+
+import com.yt.ytsearch.common.BaseResponse;
+import com.yt.ytsearch.common.ErrorCode;
+import com.yt.ytsearch.common.ResultUtils;
+import com.yt.ytsearch.exception.BusinessException;
+import com.yt.ytsearch.model.dto.postthumb.PostThumbAddRequest;
+import com.yt.ytsearch.model.entity.User;
+import com.yt.ytsearch.service.PostThumbService;
+import com.yt.ytsearch.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/DTNightWatchman">YTbaiduren</a>
+ * @from
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ YTAggSearch-new/yt-search-backend/src/main/java/com/yt/ytsearch/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yt.ytsearch.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yt.ytsearch.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/DTNightWatchman">YTbaiduren</a>
+ * @from
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
