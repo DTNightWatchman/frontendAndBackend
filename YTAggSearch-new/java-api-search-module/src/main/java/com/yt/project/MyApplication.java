ea9b706a03e6b4da64fa2c698f19746063e89bc1
lenovo <2975545458@qq.com>
2023-05-19 15:31:52 +0800

    Java的api文档搜索模块

---
+++ YTAggSearch-new/java-api-search-module/src/main/java/com/yt/project/MyApplication.java
@@ -0,0 +1,15 @@
+package com.yt.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
