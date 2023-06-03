a676354f4d44a716fd346ed100399286fdb2d485
lenovo <2975545458@qq.com>
2023-06-04 01:01:24 +0800

    建表语句

---
+++ YTAggSearch-new/java-api-search-module/sql/create_table.sql
@@ -0,0 +1,15 @@
+-- 创建库
+create database if not exists db_search;
+
+-- 切换库
+use db_search;
+
+create table doc_info
+(
+    id      int auto_increment comment '索引id'
+        primary key,
+    url     varchar(255) not null comment 'url路径',
+    title   varchar(255) null comment '文章标题',
+    content mediumtext   null comment '文章内容'
+)
+    comment '存储Java的api文档信息';
